f3a87c2e3320ae8cf359b7756c121c06f6b6e332
imtherealk <knydpdltm@naver.com>
2015-01-12 15:39:32 +0900

    Define models

--- mini/settings.py
+++ mini/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+SANDBOX = os.path.join(BASE_DIR, 'sandbox')
+
+if not os.path.exists(SANDBOX):
+    os.makedirs(SANDBOX)
 
 
 # Quick-start development settings - unsuitable for production
@@ -60,7 +64,7 @@ WSGI_APPLICATION = 'mini.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'sandbox', 'db.sqlite3'),
     }
 }
 
@@ -82,3 +86,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_PROFILE_MODULE = 'mini.web.models.UserProfile'
--- mini/web/models.py
+++ mini/web/models.py
@@ -1,1 +1,57 @@
+import os
+
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    RELATIONSHIP_STATUSES = (
+        ('y', 'Yes'),
+        ('n', 'No'),
+        ('m', 'Married'),
+    )
+
+    user = models.ForeignKey(User, unique=True)
+    relationship_status = models.CharField(max_length=1, null=True,
+                                           choices=RELATIONSHIP_STATUSES)
+    IMAGE_DIR = os.path.join(settings.SANDBOX, 'profile-images')
+    profile_image = models.ImageField(upload_to=IMAGE_DIR, null=True,
+                                      blank=True)
+
+
+class Post(models.Model):
+    writer = models.ForeignKey(User)
+    content = models.TextField(null=False)
+    created = models.DateTimeField(auto_now_add=True)
+    IMAGE_DIR = os.path.join(settings.SANDBOX, 'post-images')
+    image = models.ImageField(upload_to=IMAGE_DIR, null=True, blank=True)
+
+
+class Comment(models.Model):
+    writer = models.ForeignKey(User)
+    content = models.TextField(null=False)
+    created = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post)
+
+
+class Friend(models.Model):
+    first_user = models.ForeignKey(User, related_name='+')
+    second_user = models.ForeignKey(User, related_name='+')
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class FriendRequest(models.Model):
+    from_user = models.ForeignKey(User, related_name='+')
+    to_user = models.ForeignKey(User, related_name='+')
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class PostLike(models.Model):
+    user = models.ForeignKey(User)
+    post = models.ForeignKey(Post)
+
+
+class CommentLike(models.Model):
+    user = models.ForeignKey(User)
+    comment = models.ForeignKey(Comment)
