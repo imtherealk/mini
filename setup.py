5795126aa0d27164de047fed5818a919109671c7
imtherealk <knydpdltm@naver.com>
2015-01-12 14:51:43 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name='Mini',
+    version='0.1.0',
+    packages=setuptools.find_packages(),
+    url='https://www.github.com/imtherealk/mini',
+    license='MIT License',
+    author='realk',
+    author_email='imtherealk@gmail.com',
+    description='Mini facebook',
+    install_requires=['django'],
+)
